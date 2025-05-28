6783e07c53567c17afdba39c037bad06b0dd3918
Gaby <gabbiebrabo@gmail.com>
2025-05-28 14:52:51 -0400

    feat: feito parte da movimentação

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TupiBonito.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TupiBonito.java
@@ -3,9 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name="ja que ", group="ja ta")
+@TeleOp(name="Teste de enviar")
 
 public class TupiBonito extends LinearOpMode {
 
@@ -17,18 +18,66 @@ public class TupiBonito extends LinearOpMode {
     private DcMotor RightFront  ;
     private DcMotor RightBack  ;
 
+    // Motores garra
+    private DcMotor CasUp;
+    private DcMotor DeslizaClaw;
+    private DcMotor ColetaClaw;
 
-    private DC
+    // Servos Garra
+
+    private Servo HorizontalServ;
+    private Servo VerticalServ;
+
+
 
 
     @Override
     public void runOpMode() {
-    // declaração dos valores dos motores
+    // Declaração dos valores dos motores
 
+        LeftFront  = hardwareMap.get(DcMotor.class, "LF");
+        LeftBack  = hardwareMap.get(DcMotor.class, "LB");
+        RightFront  = hardwareMap.get(DcMotor.class, "RF");
+        RightBack  = hardwareMap.get(DcMotor.class, "RB");
+
+       //Declaração dos valores de garra
+        CasUp = hardwareMap.get(DcMotor.class,"CasUp");
+        DeslizaClaw = hardwareMap.get(DcMotor.class,"DeslizaClaw");
+        ColetaClaw = hardwareMap.get(DcMotor.class,"ColetaClaw");
+
+
+        LeftFront.setDirection(DcMotor.Direction.REVERSE);
+        LeftBack.setDirection(DcMotor.Direction.REVERSE);
+        RightFront.setDirection(DcMotor.Direction.FORWARD);
+        RightBack.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
         while (opModeIsActive()) {
+            double max;
+
+            //Movimentação do robô
+            double Vertical = gamepad1.right_stick_y;
+            double Horizontal = - gamepad1.right_stick_x;
+            double Pivot = gamepad1.left_stick_x;
+
+            //Garras no gamepad
+            float CascadingUp = gamepad2.left_trigger;
+            float CascadingDown = gamepad2.right_trigger;
+
+
+
+            RightFront.setPower((-Pivot+(Horizontal-Vertical)));
+            RightBack.setPower((-Pivot+(Horizontal+Vertical)));
+            LeftFront.setPower((Pivot+(Horizontal+Vertical)));
+            LeftBack.setPower((Pivot+(Horizontal-Vertical)));
+
+
+            CasUp.setPower(CascadingUp);
+            CasUp.setPower(-CascadingDown);
+
+
             //logica do codigo
+
 
         }
     }
