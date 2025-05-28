a28921cb6462253ed02e15c1063e36fefdd5f166
Gaby <gabbiebrabo@gmail.com>
2025-05-28 11:05:06 -0400

    feat: criação do projeto para bonito

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TupiBonito.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name="ja que ", group="ja ta")
+
+public class TupiBonito extends LinearOpMode {
+
+    //Instanciado variaveis
+
+    // Declaração de motores de movimentação
+    private DcMotor LeftFront;
+    private DcMotor LeftBack  ;
+    private DcMotor RightFront  ;
+    private DcMotor RightBack  ;
+
+
+    private DC
+
+
+    @Override
+    public void runOpMode() {
+    // declaração dos valores dos motores
+
+
+        waitForStart();
+        while (opModeIsActive()) {
+            //logica do codigo
+
+        }
+    }
+
+}
